f9c115f6543d89f53fe291f7e911f0fbff8d62ea
Erick <esantostaype@gmail.com>
2024-11-01 19:18:10 -0500

    colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,17 @@ export default {
 				inria: ['"Inria Serif"', 'serif'],
 			},
 			colors: {
-        accent: 'var(--accent)'
+        accent50: 'var(--accent-50)',
+        accent100: 'var(--accent-100)',
+        accent200: 'var(--accent-200)',
+        accent300: 'var(--accent-300)',
+        accent400: 'var(--accent-400)',
+        accent500: 'var(--accent-500)',
+        accent600: 'var(--accent-600)',
+        accent700: 'var(--accent-700)',
+        accent800: 'var(--accent-800)',
+        accent900: 'var(--accent-900)',
+        accent950: 'var(--accent-950)'
 			}
 		},
 	},
